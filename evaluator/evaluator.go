83d7c0096fde50190d19298a064c9b73183dd475
ghun131 <hungdh131@gmail.com>
2025-01-26 19:17:50 +0700

    feat: parse and evaluate a string

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -83,6 +83,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 		return applyFunction(function, args)
 
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
